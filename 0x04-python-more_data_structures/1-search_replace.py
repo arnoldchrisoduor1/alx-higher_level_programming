89053633ec98de8ddc98e17313fc1b57217ccc98
Arnold Oduor <109024629+arnoldchrisoduor1@users.noreply.github.com>
2022-12-09 14:35:28 +0300

    last update

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,32 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-
-      # Create a new list with the same size as the input list
-
-        new_list = [0 for i in range(len(my_list))]
-
-
-
-          # Loop through each element in the input list
-
-            for i in range(len(my_list)):
-
-                    # If the element is equal to the search element, add the replace element to the new list
-
-                        if my_list[i] == search:
-
-                                  new_list[i] = replace
-
-                                      # Otherwise, add the element from the input list to the new list
-
-                                          else:
-
-                                                    new_list[i] = my_list[i]
-
-
-
-                                                      # Return the new list
-
-                                                        return new_list
-
-
+    new_list = list(map(lambda x: replace if x == search else x, my_list))
+    return (new_list)
