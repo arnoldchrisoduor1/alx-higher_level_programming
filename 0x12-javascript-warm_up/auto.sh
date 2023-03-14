926b786ffd9796ef0b6ab7554d863513db337cd9
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2023-03-14 15:31:31 -0700

    task update

---
+++ 0x12-javascript-warm_up/auto.sh
@@ -0,0 +1,20 @@
+echo "<>" > 1-multi_languages.js
+echo "<>" > 2-arguments.js
+echo "<>" > 3-value_argument.js
+echo "<>" > 4-concat.js
+echo "<>" > 5-to_integer.js
+echo "<>" > 6-multi_languages_loop.js
+echo "<>" > 7-multi_c.js
+echo "<>" > 8-square.js
+echo "<>" > 9-add.js
+echo "<>" > 10-factorial.js
+echo "<>" > 11-second_biggest.js
+echo "<>" > 12-object.js
+echo "<>" > 13-add.js
+echo "<>" > 100-let_me_const.js
+echo "<>" > 101-call_me_moby.js
+echo "<>" > 102-add_me_maybe.js
+echo "<>" > 103-object_fct.js
+
+git add .
+git commit -m "task update"
