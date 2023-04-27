03d50955762038c6b0a8fba597a8ed65d41106cf
arnoldchrisoduor1 <arnoldchrisoduor@gmail.com>
2023-04-27 03:07:03 -0700

    initial commit

---
+++ auto.sh
@@ -0,0 +1,16 @@
+mkdir 0x14-javascript-web_scraping
+cd 0x14-javascript-web_scraping
+
+echo "<>" > 0-readme.js
+echo "<>" > 1-writeme.js
+echo "<>" > 2-statuscode.js
+echo "<>" > 3-starwars_title.js
+echo "<>" > 4-starwars_count.js
+echo "<>" > 5-request_store.js
+echo "<>" > 6-completed_tasks.js
+echo "<>" > 100-starwars_characters.js
+echo "<>" > 101-starwars_characters.js
+
+git add .
+git commit -m "initial commit"
+git push
